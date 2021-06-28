490a50006b50eeb2f91c74703ed37529e50ca454
elvis <mastalirs.elvis@gmail.com>
2021-06-28 09:11:09 +0300

    project

---
+++ src/main/java/com/team3/WMsystem/WMsystemApplication.java
@@ -0,0 +1,13 @@
+package com.team3.WMsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WMsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WMsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team3/WMsystem/WMsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.WMsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WMsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
